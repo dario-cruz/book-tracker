d3eddc2adee8d130bcba8b19a9f2577b1bfe15c7
Dario Cruz <rio2012@gmail.com>
2022-10-30 07:10:18 -0400

    Add for of loop to add book cards.

--- code.js
+++ code.js
@@ -25,3 +25,11 @@ function addBookToLibrary(title, author, pages, read) {
 myLibrary.push(new addBook('Game of Thrones', 'J.R.R. Martin', '500', 'No'))
 myLibrary.push(new addBook('Linchpin: How to be Indispensible', 'Seth Godin', '200', 'Yes'))
 
+for (book of myLibrary) {
+    console.log(book);
+    let elemTitle = "<p class='title'>" + book.title + "</p>"
+    let elemAuthor = "<p class='author'>" + book.author + "</p>"
+    let elemPages = "<p class='pages'>" + book.pages + "</p>"
+    let elemRead = "<p class='read'>" + book.read + "</p>"
+    library.innerHTML += "<div class='card'>" + elemTitle + elemAuthor + elemPages + elemRead + "</div>"
+}
